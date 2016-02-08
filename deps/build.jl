cfadf71427c6305c07b521adf54908e5789955d5
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-02-08 20:45:23 +0100

    build quad precision fftw

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,14 @@
 cd(dirname(@__FILE__))
+if (!ispath("fftw-3.3.4"))
+    download("http://www.fftw.org/fftw-3.3.4.tar.gz","fftw3.tgz")
+    run(`tar xzf fftw3.tgz`)
+end
+cd("fftw-3.3.4")
+install_dir = pwd()
+run(`./configure --prefix=$install_dir --enable-quad-precision --enable-openmp --enable-threads --enable-shared`)
+run(`make install`)
+cd("..")
+
 if (!ispath("TSSM"))
     run(`git clone git://github.com/HaraldHofstaetter/TSSM.git`)
 else
