7885f57084d553272f4b150ee3d2c3c93f7cafb9
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-02-08 21:45:40 +0100

    build quadprecision fftw

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,12 @@
 cd(dirname(@__FILE__))
+
+if (!ispath("usr"))
+    run(`mkdir usr`)
+end
+if (!ispath("usr/lib"))
+    run(`mkdir usr/lib`)
+end
+
 if (!ispath("fftw-3.3.4"))
     download("http://www.fftw.org/fftw-3.3.4.tar.gz","fftw3.tgz")
     run(`tar xzf fftw3.tgz`)
@@ -6,7 +14,7 @@ if (!ispath("fftw-3.3.4"))
 end
 install_dir = pwd()
 cd("fftw-3.3.4")
-run(`./configure --prefix=$install_dir --enable-quad-precision --enable-openmp --enable-threads --enable-shared`)
+run(`./configure --prefix=$install_dir/usr --enable-quad-precision --enable-openmp --enable-threads --enable-shared`)
 run(`make install`)
 cd("..")
 rm("fftw-3.3.4", recursive=true)
@@ -21,19 +29,16 @@ end
 
 if searchindex(readall(`uname -a`), "juliabox")>0
     run(`make -C TSSM/OPENMP JULIABOX_BUILD=1 all`)
+    run(`mv TSSM/OPENMP/libtssm.$(Libdl.dlext) usr/lib`)
+    run(`make -C TSSM/OPENMP clean`)
     run(`make -C TSSM/QUADPREC_OPENMP JULIABOX_BUILD=1 all`)
+    run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
+    run(`make -C TSSM/QUADPREC_OPENMP clean`)
 else    
     run(`make -C TSSM/OPENMP all`)
     run(`make -C TSSM/QUADPREC_OPENMP all`)
+    run(`mv TSSM/OPENMP/libtssm.$(Libdl.dlext) usr/lib`)
+    run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
 end
 
-if (!ispath("usr"))
-    run(`mkdir usr`)
-end
-if (!ispath("usr/lib"))
-    run(`mkdir usr/lib`)
-end
-
-run(`mv TSSM/OPENMP/libtssm.$(Libdl.dlext) usr/lib`)
-run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
 
