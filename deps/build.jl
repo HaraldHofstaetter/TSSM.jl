b20e6283d2b95d08d22d90de24d320c616bf0676
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-02-09 13:11:15 +0100

    Float128 support only optional

--- deps/build.jl
+++ deps/build.jl
@@ -7,20 +7,6 @@ if (!ispath("usr/lib"))
     run(`mkdir usr/lib`)
 end
 
-if (   !ispath("usr/lib/libfftw3q.so.3.4.4")
-    || !ispath("usr/lib/libfftw3q_omp.so.3.4.4"))
-    if (!ispath("fftw-3.3.4"))
-        download("http://www.fftw.org/fftw-3.3.4.tar.gz","fftw3.tgz")
-        run(`tar xzf fftw3.tgz`)
-        rm("fftw3.tgz")
-    end
-    install_dir = pwd()
-    cd("fftw-3.3.4")
-    run(`./configure --prefix=$install_dir/usr --enable-quad-precision --enable-openmp --enable-threads --enable-shared`)
-    run(`make install`)
-    cd("..")
-    rm("fftw-3.3.4", recursive=true)
-end
 
 if (!ispath("TSSM"))
     run(`git clone git://github.com/HaraldHofstaetter/TSSM.git`)
@@ -34,14 +20,43 @@ if searchindex(readall(`uname -a`), "juliabox")>0
     run(`make -C TSSM/OPENMP JULIABOX_BUILD=1 all`)
     run(`mv TSSM/OPENMP/libtssm.$(Libdl.dlext) usr/lib`)
     run(`make -C TSSM/OPENMP clean`)
-    run(`make -C TSSM/QUADPREC_OPENMP JULIABOX_BUILD=1 all`)
-    run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
-    run(`make -C TSSM/QUADPREC_OPENMP clean`)
 else    
     run(`make -C TSSM/OPENMP all`)
     run(`make -C TSSM/QUADPREC_OPENMP all`)
-    run(`mv TSSM/OPENMP/libtssm.$(Libdl.dlext) usr/lib`)
-    run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
 end
+
+
+use_Float128 = false
+try
+    using Quadmath
+    use_Float128 = true
+end
+
+if use_Float128
+    if (   !ispath("usr/lib/libfftw3q.so.3.4.4")
+        || !ispath("usr/lib/libfftw3q_omp.so.3.4.4"))
+        if (!ispath("fftw-3.3.4"))
+            download("http://www.fftw.org/fftw-3.3.4.tar.gz","fftw3.tgz")
+            run(`tar xzf fftw3.tgz`)
+            rm("fftw3.tgz")
+        end
+        install_dir = pwd()
+        cd("fftw-3.3.4")
+        run(`./configure --prefix=$install_dir/usr --enable-quad-precision --enable-openmp --enable-threads --enable-shared`)
+        run(`make install`)
+        cd("..")
+        rm("fftw-3.3.4", recursive=true)
+    end
+
+    if searchindex(readall(`uname -a`), "juliabox")>0
+        run(`make -C TSSM/QUADPREC_OPENMP JULIABOX_BUILD=1 all`)
+        run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
+        run(`make -C TSSM/QUADPREC_OPENMP clean`)
+    else    
+        run(`make -C TSSM/QUADPREC_OPENMP all`)
+        run(`mv TSSM/QUADPREC_OPENMP/libtssmq.$(Libdl.dlext) usr/lib`)
+    end
+end
+
 
 
